44ca2b2e8f69cf63835f5161b887f517409bd4a7
Sifat Khan <khansifatcstu@gmail.com>
2026-03-10 17:48:09 +0600

    Add age calculation script for remaining time

---
+++ Exercise of Playlist/Exercise 5 | Python for Beginners.py
@@ -0,0 +1,6 @@
+age = int(input('age:'))
+years_left=90-age
+days_left=years_left*365
+weeks_left=years_left*52
+months_left=years_left*12
+print(f'You have {days_left} days,{weeks_left} weeks and {months_left} months left')
